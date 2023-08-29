0db410e7f222b8b399b9d9350fd9ba4d2087c399
RUPALI ARUN RAKHUNDE <98753140+12345rupali@users.noreply.github.com>
2023-08-29 18:55:25 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*typing animation */
+
+var typed = new Typed('.typing', {
+    strings: ['Web Developer', 'Graphics Designer', 'ML Engineer', 'Software Developer', 'Data Analytics'],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
